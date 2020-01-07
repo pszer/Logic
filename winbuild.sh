c02a420a0c92b435b7b1ca0284bec97801744e4e
pszerszynski <alienmode1337@gmail.com>
2020-01-07 18:03:29 +0000

    commit

--- winbuild.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-i686-w64-mingw32-gcc -mwindows src/*.c -o logic.exe -Iinclude/ -ISDL2/include -ISDL2_image/include -ISDL2_ttf/include -L. -D__MINGW32__ -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_image -lpthread
-
